4d621a409562ee1b3c533cb831315b54cd415ee2
vignesh <ssaivignesh7@gmail.com>
2023-01-05 12:57:44 +0530

    Add project files.

---
+++ MessageConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MessageLibrary;
+namespace MessageConsoleApp
+{
+    class MessageConsole
+    {
+        static void Main(string[] args)
+        {
+            var t = new MessageClass();
+            t.Message();
+        }
+    }
+}
---
+++ MessageLibrary/MessageClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MessageLibrary
+{
+    public class MessageClass
+    {
+        public void Message()
+        {
+            Console.WriteLine("I am great");
+        }
+    }
+}
